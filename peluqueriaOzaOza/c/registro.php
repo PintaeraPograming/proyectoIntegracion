c20008a9c1dd655daf3f9a68ab6eebd310d0b71e
Fabricio Damián Rojas Cuomo <fabriciorojascuomo@outlook.es>
2016-03-14 11:17:35 +0000

    cambios

--- peluqueriaOzaOza/c/registro.php
+++ peluqueriaOzaOza/c/registro.php
@@ -17,15 +17,14 @@
 	// Se llama a la funcion setUsuario.
 	// Se crea un usuario nuevo con los datos pasados por parametro.
 	// Se redirecciona al inicio.
-	if(!preg_match('/^[9|6|7][0-9]{8}$/', $movil)){
-		$movil = "";
-		echo "<script>alert('Numero de movil feo');</script>";
-	}
 	if($user != "" && $pass != "" && $movil != ""){
-		
-		setUsuario($user, md5($pass), $nombre, $apellidos, $telefono , $movil);
-		$_SESSION ["usuario"] = $user;
-		header('Location: menu.php');
+		if(preg_match('/^[9|6|7][0-9]{8}$/', $movil)){
+			setUsuario($user, md5($pass), $nombre, $apellidos, $telefono , $movil);
+			$_SESSION ["usuario"] = $user;
+			header('Location: menu.php');
+		}else{
+			header('Location: registro.php');
+		}
 	}
 	
 	// Funcionalidad de los botones.
